3ae70be92ebed2c98e8c4f53a98c0dffe09a5cb0
marwa <mds08@mail.aub.edu>
2023-01-08 15:27:30 +0200

    navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,16 @@ import '../styles/globals.css'
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
+import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
-
     }
     )},[])
   return (
     <>
+      <Navbar/>
       <Component {...pageProps} />
     </>
   )
