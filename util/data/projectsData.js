ca71d8ae9ee1c5502b42b54fc456c8ec85c3b372
marwa <mds08@mail.aub.edu>
2023-01-16 15:04:49 +0200

    data update

--- util/data/projectsData.js
+++ util/data/projectsData.js
@@ -3,7 +3,7 @@ export const projectsData = [
         title:"Jobie",
         link:"https://capstonejobie.vercel.app",
         src:"/jobie.png",
-        text:"Containing 5 responsive pages as well as a user profile, 'Jobie' was the culmination of work of a 17-membered team and I for our capstone project at Re:Coded's front-end development bootcamp. As it was my first time working on a tech project with a big team, I have learnt valuable lessons. I had the chance to participate in all the phases of the project contributing to the set-up, design, functionality and bug-bashing. We used multiple frameworks and libraries to make our ideas come to life, including: react, redux-tool kit, i18next, firebase, tailwind, jest, toastify, yup, and react-icons.",
+        text:"Containing five responsive pages as well as a user profile, 'Jobie' was the culmination of work of a 17-membered team and I for our capstone project at Re:Coded's front-end development bootcamp. As it was my first time working on a tech project with a big team, I have learnt valuable lessons. I had the chance to participate in all the phases of the project contributing to the set-up, design, functionality and bug-bashing. We used multiple frameworks and libraries to make our ideas come to life, including: react, redux-tool kit, i18next, firebase, tailwind, jest, toastify, yup, and react-icons.",
         path:"Jobie",
         image:'/jobiesite.png',
        
