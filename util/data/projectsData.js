42679b338eb7f28ce0bf49fb5df66bf235fd68d3
Marwa Salameh <108797405+marwazpov@users.noreply.github.com>
2023-02-21 23:15:31 +0200

    Update projectsData.js

--- util/data/projectsData.js
+++ util/data/projectsData.js
@@ -6,6 +6,7 @@ export const projectsData = [
         text:"Containing five responsive pages as well as a user profile, 'Jobie' was the culmination of work of a 17-membered team and I for our capstone project at Re:Coded's front-end development bootcamp. As it was my first time working on a tech project with a big team, I have learnt valuable lessons. I had the chance to participate in all the phases of the project contributing to the set-up, design, functionality and bug-bashing. We used multiple frameworks and libraries to make our ideas come to life, including: react, redux-tool kit, i18next, firebase, tailwind, jest, toastify, yup, and react-icons.",
         path:"Jobie",
         image:'/jobiesite.png',
+        code:"https://github.com/ReCoded-Org/-capstone-IQLBPS-jobie",
        
     },
 
@@ -16,6 +17,7 @@ export const projectsData = [
         text:"This project fetches data from The Movie Database (TMDB) API, displaying movies, casts, ratings, trailers, related movies, genres and so on. Users can navigate through the site, picking to sort movies with different filters as well as learn more about their favorite actors. As this project was built early on in my learning process, it utilized object-oriented programming (OOP).",
         path:'TheMovieProject',
         image:'/moviesite.png',
+        code:"https://github.com/marwazpov/movie-project",
     },
 
 ]
